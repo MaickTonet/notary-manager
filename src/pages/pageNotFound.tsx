422cfea5c1d2c00fc7390a054b678e3de7b4a7fc
Maick Tonet <maicktonet4@gmail.com>
2024-09-03 21:09:52 -0300

    Add page not found

--- src/pages/pageNotFound.tsx
+++ src/pages/pageNotFound.tsx
@@ -1,9 +1,24 @@
+import { LogIn } from "lucide-react";
+import { Link } from "react-router-dom";
 export default function PageNotFound() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        A pagina atual não existe
-      </h1>
+    <div className="bg-primary h-screen flex flex-col justify-center items-center p-2">
+      <img
+        src="src/assets/page-not-found.svg"
+        alt="Page not found illustration"
+        className="w-full md:w-1/3"
+      />
+      <div className="flex flex-col gap-5 items-center ">
+        <h1 className="font-bold text-4xl text-center">
+          Página não encontrada
+        </h1>
+        <Link to={"/home"}>
+          <button className="flex gap-3 items-center  border border-black/50 p-3 font-bold text-xl w-fit rounded-md shadow hover:scale-105        transition-all hover:shadow-md">
+            <LogIn />
+            Voltar para o início
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
